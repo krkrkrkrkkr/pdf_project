7c0a64da6f97e1dd4fb16a4afbbb685b46025355
krkrkrkrkkr <krishivgupta@gmail.com>
2022-11-04 19:38:36 +0530

    adding the file to repository

---
+++ pdf_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import FileResponse, Http404
+
+# Create your views here.
+def home(request):
+    try:
+        return FileResponse(open('readme.pdf','rb'),content_type="application/pdf")
+    except FileNotFoundError:
+        raise Http404()
+
