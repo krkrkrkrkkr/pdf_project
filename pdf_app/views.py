608152f6704d56134fb1aaece01f9d9a49028fd2
krkrkrkrkkr <krishivgupta@gmail.com>
2022-11-04 19:47:09 +0530

    changed views

--- pdf_app/views.py
+++ pdf_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import FileResponse, Http404
-
+import os
+from django.conf import settings
 # Create your views here.
 def home(request):
     try:
-        return FileResponse(open('readme.pdf','rb'),content_type="application/pdf")
+        filepath=os.path.join(settings.BASE_DIR,"readme.pdf")
+        return FileResponse(open(filepath,'rb'),content_type="application/pdf")
     except FileNotFoundError:
         raise Http404()
 
